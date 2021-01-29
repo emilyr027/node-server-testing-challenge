1e7e425e7036474779cb681dba8a374d377bed0f
Emily Ryan <emilyryan@Emilys-MacBook-Pro.local>
2021-01-28 18:59:40 -0600

    created todos model

--- todos/todos-model.js
+++ todos/todos-model.js
@@ -1,2 +1,27 @@
 const db = require('../data/dbConfig')
 
+module.exports = {
+    getAll,
+    findById,
+    insert,
+    remove
+  }
+  
+  function getAll() {
+    return db('todos')
+  }
+  
+  function findById(id) {
+    return db('todos').where({ id }).first()
+  }
+  
+  function insert(data) {
+    return db('todos')
+    .insert(data, 'id')
+    .then(ids => ({ id: ids[0], ...data}))
+  }
+  
+  function remove(id) {
+    return db('todos').where('id', id).del()
+  }
+  
